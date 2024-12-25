ef0b2d57fea10a7a20d1545de8a7ce6da040a1a9
Condy <132919366+yu03-dev@users.noreply.github.com>
2024-12-25 19:33:48 +0900

    keymap削除

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,6 +3,3 @@
 -- Add any additional keymaps here
 
 vim.keymap.set("i", "jj", "<ESC>", { noremap = true, silent = true })
-
-vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save file" })
-vim.keymap.set("n", "<leader>q", ":q<CR>", { desc = "Quit file" })
